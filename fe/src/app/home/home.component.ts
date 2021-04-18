5540b8e318f52d9f51617dedd166e5bde35de4be
Tamara Mayer <tamara-mayer@outlook.com>
2021-04-18 19:13:00 +0200

    added boolean to homepage

--- fe/src/app/home/home.component.ts
+++ fe/src/app/home/home.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '@app/_services/user.service';
 
 @Component({ templateUrl: 'home.component.html' })
-export class HomeComponent { }
+export class HomeComponent implements OnInit  {
+    public loggedIn:boolean;
+
+    constructor(
+        private userService: UserService) {
+            this.loggedIn = false;
+        }
+ 
+        ngOnInit(): void {
+            if(this.userService.UserData!=null){
+                this.loggedIn = true;
+            }
+        }
+       }
